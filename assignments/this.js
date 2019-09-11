b66c6a501e39091298110361d9152cc4a5f5066c
c00kamunga <tsothalanti2@yahoo.com>
2019-09-10 17:43:27 -0700

    answered the 4 principles of the this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding - "this" is the window object; we can "use strict" to bypass this.
+
+* 2. implicit binding - This is essentially dot syntax, whatever is to the elft of the dot is "this".
+
+* 3. explicit binding - We are stating what we want "this" to be through the use of functions.
+
+* 4. new keyword - Serves as the bridge between constructors and dot syntax. It allows us to use the same template to create more objects.
 *
 * write out a code example of each explanation above
 */
@@ -52,15 +55,6 @@ function Being(tasty){
 }
 const human = new Being("burgers");
 human.eat();
-
-
-
-
-
-
-
-
-
 
 
 
